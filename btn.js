008799961a799f79acf9fb3416bc12129341baf8
flipfriend <ahill7.ah@gmail.com>
2020-01-05 09:29:45 -0800

    added apend to end of btn function

--- btn.js
+++ btn.js
@@ -6,9 +6,10 @@ $(document).ready(function () {
 
     $('#pBtn').click(function () {
         
-        $('.partyInvite').add('<p class="addon">Let\'s party. Join us at the famous Saint Rocke in hermosa beach to celebrate 50 weird years. Enjoy a few hours of some of our favorite rock,dance funk, and pop songs. Played by the group of some of LA\'s finest session musicians. Eat, drink, eat, drink some more. Bang your head. Shake your booty. See you there. <br> - Chris & Kimm</p>')
+        $('.partyInvite').add('<p class="addon">Let\'s party. Join us at the famous Saint Rocke in hermosa beach to celebrate 50 weird years. Enjoy a few hours of some of our favorite rock,dance funk, and pop songs. Played by the group of some of LA\'s finest session musicians. Eat, drink, eat, drink some more. Bang your head. Shake your booty. See you there. <br> - Chris & Kimm</p>').appendTo(document.body)
 
             $('.porbtn').toggleClass('partyInvite');
+            console.log('added message')
     })
 
 });
