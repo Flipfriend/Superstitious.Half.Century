c13bf58e04f43ca237af1802f0d5d1d41db2f8cf
flipfriend <ahill7.ah@gmail.com>
2020-01-05 09:31:04 -0800

    removed append added before

--- btn.js
+++ btn.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 
     $('#pBtn').click(function () {
         
-        $('.partyInvite').add('<p class="addon">Let\'s party. Join us at the famous Saint Rocke in hermosa beach to celebrate 50 weird years. Enjoy a few hours of some of our favorite rock,dance funk, and pop songs. Played by the group of some of LA\'s finest session musicians. Eat, drink, eat, drink some more. Bang your head. Shake your booty. See you there. <br> - Chris & Kimm</p>').appendTo(document.body)
+        $('.partyInvite').before('<p class="addon">Let\'s party. Join us at the famous Saint Rocke in hermosa beach to celebrate 50 weird years. Enjoy a few hours of some of our favorite rock,dance funk, and pop songs. Played by the group of some of LA\'s finest session musicians. Eat, drink, eat, drink some more. Bang your head. Shake your booty. See you there. <br> - Chris & Kimm</p>')
 
             $('.porbtn').toggleClass('partyInvite');
             console.log('added message')
