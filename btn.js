ce75f3a64809692dfa2c0fbe0699d7df8d8c54fd
flipfriend <ahill7.ah@gmail.com>
2020-01-05 09:57:20 -0800

    changed btn function to toggle show and hide added .addon style

--- btn.js
+++ btn.js
@@ -6,9 +6,7 @@ $(document).ready(function () {
 
     $('#pBtn').click(function () {
         
-        $('.partyInvite').show('<p class=".addon">Let\'s party. Join us at the famous Saint Rocke in hermosa beach to celebrate 50 weird years. Enjoy a few hours of some of our favorite rock,dance funk, and pop songs. Played by the group of some of LA\'s finest session musicians. Eat, drink, eat, drink some more. Bang your head. Shake your booty. See you there. <br> - Chris & Kimm</p>')
-
-            $('.porbtn').toggleClass('partyInvite');
+            $('.porbtn').toggleClass('.addon');
             console.log('added message')
     })
 
